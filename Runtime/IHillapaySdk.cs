dc80357757eaf06b9fe5ca55d57ac98bfe0496f1
m.jafari <mehrtarh@yahoo.com>
2020-07-07 14:19:49 +0430

    Add sdk interface

---
+++ Runtime/IHillapaySdk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHillapaySdk
+{
+    void paymentResult(String ipgModel);
+    void verifyResult(String verifyModel);
+    void directDebitResult(String payModel);
+    void ipgReportResult(String reportModel);
+    void ipgReportLastListResult(String reportModel);
+    void failedSDK(String errorModel);
+}
